12d87a2a0d1180c62e2dc5025e7ea497a565f48f
mawekk <mariamya243@gmail.com>
2021-09-15 21:13:32 +0300

    corrections of mistakes

--- homework_1/task_3.c
+++ homework_1/task_3.c
@@ -3,7 +3,7 @@
 
 void changeTheBeginningAndEnd(int beginning, int size, int* array)
 {
-    int element;
+    int element = 0;
 
     for (int i = beginning; i < size; ++i) {
         for (int j = i; j > i - beginning; --j) {
@@ -16,19 +16,19 @@ void changeTheBeginningAndEnd(int beginning, int size, int* array)
 
 int main()
 {
-    int m, n, size;
-    printf("Enter m:\n");
-    scanf("%d", &m);
-    printf("Enter n:\n");
-    scanf("%d", &n);
-    size = m + n;
+    int length1 = 0, length2 = 0, size = 0;
+    printf("Enter the length of the first part of the array:\n");
+    scanf("%d", &length1);
+    printf("Enter the length of the second part of the array:\n");
+    scanf("%d", &length2);
+    size = length1 + length2;
 
     int* array = calloc(size, sizeof(int));
     printf("Enter the array elements:\n");
     for (int i = 0; i < size; ++i)
         scanf("%d", &array[i]);
 
-    changeTheBeginningAndEnd(m, size, array);
+    changeTheBeginningAndEnd(length1, size, array);
 
     printf("The new array:\n");
     for (int i = 0; i < size; ++i)
