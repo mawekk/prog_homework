d1d06abbbedec82f39d7cec7dae6fbe5e2ba0bbb
mawekk <mariamya243@gmail.com>
2021-10-04 00:40:49 +0300

    hw2.2 (not done)

---
+++ homework_2/task_2.c
@@ -0,0 +1,9 @@
+#include "../library/DNA.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+        printf("Incorrect number of argument");
+    return 0;
+}
---
+++ library/DNA.h
@@ -0,0 +1,7 @@
+typedef struct MapDNA MapDNA;
+typedef struct MapElement MapElement;
+MapDNA* makeNewMap();
+MapElement* makeNewElement(MapDNA* map, const char* DNA);
+void deleteFragment(MapDNA* map, char* start, char* end);
+void insertFragment(MapDNA* map, char* start, char* fragment);
+void replaceFragment(MapDNA* map, char* template, char* fragment);
