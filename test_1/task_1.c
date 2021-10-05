5efdfe99aa1801eb2a372d1bef4786cd922d06f3
mawekk <mariamya243@gmail.com>
2021-10-05 12:48:56 +0300

    test1

--- test_1/task_1.c
+++ test_1/task_1.c
@@ -13,31 +13,31 @@ int main()
 
     printf("Enter the coordinates of the first vector:\n");
     for (int i = 0; i < size; ++i)
-        scanf("%d", firstCoordinates[i]);
+        scanf("%d", &firstCoordinates[i]);
     Vector* firstVector = makeNewVector(size, firstCoordinates);
 
     printf("Enter the coordinates of the second vector:\n");
     for (int i = 0; i < size; ++i)
-        scanf("%d", secondCoordinates[i]);
+        scanf("%d", &secondCoordinates[i]);
     Vector* secondVector = makeNewVector(size, secondCoordinates);
 
-    printf("Length of the first vector = ", vectorLength(firstVector, size));
-    printf("Length of the second vector = ", vectorLength(secondVector, size));
+    printf("Length of the first vector = %f\n", vectorLength(firstVector, size));
+    printf("Length of the second vector = %f\n", vectorLength(secondVector, size));
 
-    printf("Scalar product of the vectors = ", scalarProduct(firstVector, secondVector, size));
+    printf("Scalar product of the vectors = %d\n", scalarProduct(firstVector, secondVector, size));
 
-    printf("Sum of the vectors:\n ");
+    printf("Sum of the vectors:\n");
     int* sum = calloc(size, sizeof(int));
     additionOfVectors(firstVector, secondVector, size, sum);
     for (int i = 0; i < size; ++i)
-        printf("%d, ", sum[i]);
+        printf("%d ", sum[i]);
     printf("\n");
 
-    printf("Difference of the vectors:\n ");
+    printf("Difference of the vectors:\n");
     int* difference = calloc(size, sizeof(int));
     vectorDifference(firstVector, secondVector, size, difference);
     for (int i = 0; i < size; ++i)
-        printf("%d, ", difference[i]);
+        printf("%d ", difference[i]);
     printf("\n");
 
     return 0;
