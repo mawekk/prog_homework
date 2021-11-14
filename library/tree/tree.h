e804d3da3c2f286ddf0a2481916912960ee39e93
mawekk <mariamya243@gmail.com>
2021-11-15 00:13:04 +0300

    idk :))

---
+++ library/tree/tree.h
@@ -0,0 +1,20 @@
+#ifndef TREE_H
+#define TREE_H
+#include "../values/values.h"
+#include <stdlib.h>
+
+typedef struct Node Node;
+typedef struct TreeMap TreeMap;
+typedef struct Pair {
+    Value key;
+    Value value;
+} Pair;
+
+TreeMap* createTreeMap(Comparator comparator);
+void deleteTreeMap(TreeMap* map);
+void putKey(TreeMap* tree, Value key, Value value);
+Pair removeKey(TreeMap* tree, Value key);
+bool hasKey(TreeMap* tree, Value key);
+
+
+#endif // TREE_H
